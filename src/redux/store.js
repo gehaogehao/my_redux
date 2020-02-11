5e41ad05b70d752e06604fbb2af0f28381d6454c
gehao <1073198660@qq.com>
2020-02-11 20:06:18 +0800

    ok

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,5 +3,5 @@
 import {createStore} from 'redux'
 //引入reducer,操作状态
 import countReducer from './count_reducer'
-//调用createStore时，必须传入一个reducer   上菜
+//调用createStore时，必须传入一个reducer   (用于接收reducer返回的新状态newState)
 export default createStore(countReducer)
