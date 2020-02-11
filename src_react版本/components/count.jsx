1c83362dc6226247cb41e2347d61b0f427c9e2c5
gehao <1073198660@qq.com>
2020-02-11 19:05:17 +0800

    ok

---
+++ src_react版本/components/count.jsx
@@ -0,0 +1,54 @@
+import React, { Component } from 'react'
+
+export default class Count extends Component {
+
+    state={
+        number:0
+    }
+
+    //加
+    increment=()=>{
+        const value = this.refs.number.value
+        let number = this.state.number+value*1
+        this.setState({number})
+    }
+    //减
+    decrement=()=>{
+        const value = this.refs.number.value
+        let number = this.state.number-value*1
+        this.setState({number})
+    }
+    //奇数加
+    incrementOdd=()=>{
+        const value = this.refs.number.value
+        let number = this.state.number
+        if(number%2 === 1){
+            this.setState({number:number+value*1})
+        }
+    }
+    //延时加
+    incrementAsync=()=>{
+        const value = this.refs.number.value
+        let number = this.state.number+value*1
+        setTimeout(()=>{
+            this.setState({number})
+        },1000)
+    }
+
+    render() {
+        return (
+            <div>
+                <h2>计数器:当前数字为{this.state.number}</h2>
+                <select ref='number'>
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                </select>&nbsp;&nbsp;
+                <button onClick={this.increment}>+</button>&nbsp;&nbsp;
+                <button onClick={this.decrement}>-</button>&nbsp;&nbsp;
+                <button onClick={this.incrementOdd}>increment if odd</button>&nbsp;&nbsp;
+                <button onClick={this.incrementAsync}>increment async</button>
+            </div>
+        )
+    }
+}
